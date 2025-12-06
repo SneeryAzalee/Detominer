b414207018deb98d587363213d5725a0989f2553
Russel Dean Ouano <azalee.ouano@gmail.com>
2025-12-06 10:48:10 +0800

    Modified config-opening logic to use Minecraft's official utils in client

--- src/client/java/io/sneery/detominer/client/ModMenuIntegration.java
+++ src/client/java/io/sneery/detominer/client/ModMenuIntegration.java
@@ -2,9 +2,9 @@ package io.sneery.detominer.client;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.util.Util;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.logging.Logger;
 
 public class ModMenuIntegration implements ModMenuApi {
@@ -14,26 +14,11 @@ public class ModMenuIntegration implements ModMenuApi {
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> {
             File configFile = new File("config/detominer.json");
-            String os = System.getProperty("os.name").toLowerCase();
-            String[] command;
-
-            if (os.contains("win")) {
-                command = new String[] { "explorer", configFile.getAbsolutePath() };
-            } else if (os.contains("mac")) {
-                command = new String[] { "open", configFile.getAbsolutePath() };
-            } else if (os.contains("nux") || os.contains("nix")) {
-                command = new String[] { "xdg-open", configFile.getAbsolutePath() };
-            } else {
-                LOGGER.warning("Unsupported OS for opening config file.");
-                return parent;
-            }
-
             try {
-                Runtime.getRuntime().exec(command);
-            } catch (IOException e) {
+                Util.getOperatingSystem().open(configFile);
+            } catch (Exception e) {
                 LOGGER.severe("Failed to open config file: " + e.getMessage());
             }
-
             return parent;
         };
     }
