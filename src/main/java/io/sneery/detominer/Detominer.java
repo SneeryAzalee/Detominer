8c3fd3e283d251e6c69ced85f66b4be83075d833
Russel Dean Ouano <azalee.ouano@gmail.com>
2025-12-13 00:10:52 +0800

    Lowered the minimum fabric loader version to v0.18.0

--- src/main/java/io/sneery/detominer/Detominer.java
+++ src/main/java/io/sneery/detominer/Detominer.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class Detominer implements ModInitializer {
     public static final String MOD_ID = "detominer";
     public static final String MOD_NAME = "Detominer";
-    public static final String MOD_VERSION = "4.0";
+    public static final String MOD_VERSION = "4.1";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
